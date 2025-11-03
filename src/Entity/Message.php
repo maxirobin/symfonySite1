ed408baec4cb730b250cc6014683048f5da67b3d
maximeue4 <maxirobin81@gmail.com>
2025-11-03 09:06:59 +0100

    Fin de code first

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -6,6 +6,8 @@ use App\Repository\MessageRepository;
 use DateTime;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
 class Message
@@ -24,6 +26,111 @@ class Message
     #[ORM\ManyToOne(inversedBy: 'messages')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
+
+    // Relation ManyToMany avec une collection de catégories
+    #[ORM\ManyToMany(targetEntity: Category::class)]
+    #[ORM\JoinTable(name: 'message_category')] // Table de jonction
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->creation_date = new DateTime();
+        $this->categories = new ArrayCollection(); // Initialisation de la collection
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTime
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTime $creation_date): static
+    {
+        $this->creation_date = $creation_date;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function setCategories(Collection $categories): static
+    {
+        $this->categories = $categories;
+        return $this;
+    }
+
+    // Méthodes pour ajouter et enlever des catégories
+    public function addCategory(Category $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+        return $this;
+    }
+
+    public function removeCategory(Category $category): static
+    {
+        $this->categories->removeElement($category);
+        return $this;
+    }
+}
+/*<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $body = null;
+
+    #[ORM\Column]
+    private ?\DateTime $creation_date = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne]
+    private ?Category $categories = null;
 
     public function __construct()
     {
@@ -70,4 +177,17 @@ class Message
 
         return $this;
     }
+
+    public function getCategories(): ?Category
+    {
+        return $this->categories;
+    }
+
+    public function setCategories(?Category $categories): static
+    {
+        $this->categories = $categories;
+
+        return $this;
+    }
 }
+*/
